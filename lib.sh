4b48bcc05586a3ca810dccb0d2c8dd76ee39f366
Nicolas Jalibert <njalibert@macbook.local>
2014-11-21 23:16:31 +0100

    lib.sh peut maintenant faire la différence entre base et dossiers spécifiques

--- lib.sh
+++ lib.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+#Usage: generate_command ORGDIR DSTDIR SRC FUNCTION
+generate_command(){
+    ORGDIR=$1
+    DSTDIR=$2
+    SRC=$3
+    FUNCTION=$4
+    
+    LEN=${#ORGDIR}
+    DST=$DSTDIR/${SRC:$LEN}
+    
+    OUT="$FUNCTION $DST"
+    eval $OUT
+}
+#Exemple: generate_command "/var/" "/tmp" "/var/log/daemon.log" "echo"
+
